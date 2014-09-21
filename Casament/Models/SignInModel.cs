14eaefa4d2f8132369c64643635dd01eaff01212
ALlongueras <albert@albertllongueras.cat>
2014-09-21 17:52:19 +0200

    Added structure of home page with loggin

---
+++ Casament/Models/SignInModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Casament.Models
+{
+    public class SignInModel
+    {
+        [Required, Display(Name = "User name")]
+        public string Email { get; set; }
+
+        [Required, Display(Name = "Password"), DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
