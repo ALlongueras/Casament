65c54329824fa19783b6fcbdd9c297a9af517da7
ALlongueras <albert@albertllongueras.cat>
2015-02-19 23:56:33 +0100

    Added map and assistance button

---
+++ Casament/Controllers/AssistanceController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Casament.Controllers
+{
+    using Umbraco.Core;
+    using Umbraco.Core.Services;
+    using Umbraco.Web.WebApi;
+
+    public class AssistanceController : UmbracoApiController
+    {
+        [HttpGet]
+        public bool UpdateAssistance(int id, int action)
+        {
+            var currentMember = ApplicationContext.Current.Services.MemberService.GetById(id);
+            var assistance = currentMember.Properties.FirstOrDefault(x => x.Alias == "assistance");
+            if (assistance != null)
+            {
+                var newAssistance = action == 1;
+                assistance.Value = newAssistance;
+                ApplicationContext.Current.Services.MemberService.Save(currentMember);
+                return true;
+            }
+            return false;
+        }
+
+        [HttpGet]
+        public int GetCurrentAssistance(int id)
+        {
+            var currentMember = ApplicationContext.Current.Services.MemberService.GetById(id);
+            var assistance = currentMember.Properties.FirstOrDefault(x => x.Alias == "assistance");
+            if (assistance != null)
+            {
+                return int.Parse(assistance.Value.ToString());
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ Casament/scripts/quiz/home.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#infoSection a.icon").click(function () {
+        event.preventDefault();
+        var userId = $(".quiz").data("user");
+        var action = $(this).data("value");
+        UpdateAssistance(this, userId, action);
+    });
+});
+
+function UpdateAssistance(element, userId, action) {
+    $.ajax({
+        url: "http://local.casament.com/Umbraco/Api/Assistance/UpdateAssistance?id=" + userId + "&action=" + action,
+        type: 'GET',
+        async: true,
+        dataType: "json",
+        contentType: 'application/json, charset=utf-8',
+        success: function (data) {
+            if (data == true) {
+                $("#infoSection .content .green").removeClass("green");
+                $("#infoSection .content .gray").removeClass("gray");
+                $(element).addClass("green");
+            }
+        }
+    });
+}
+
